833c8d2c5c96f7992e1a1fbedb9ec09cbd337318
Legion <xeosmarthome@gmail.com>
2020-07-31 11:47:32 +0300

    init

--- App.js
+++ App.js
@@ -1,21 +1,57 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {createMaterialBottomTabNavigator} from "react-navigation-material-bottom-tabs";
+import {createAppContainer} from "react-navigation";
+import MainScreen from "./screens/main-screen";
+import EncryptFileScreen from "./screens/encrypt-file-screen";
+import DecryptFileScreen from "./screens/decrypt-file-screen";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const BottomNavigator = createMaterialBottomTabNavigator(
+	{
+		home:{
+			screen: MainScreen,
+			navigationOptions:{
+				tabBarLabel: 'My files',
+				tabBarIcon: ({tintColor}) => (
+					<Icon name="home" color={tintColor} size={24} />
+				)
+			}
+		},
+		encrypt_file:{
+			screen: EncryptFileScreen,
+			navigationOptions:{
+				tabBarLabel: 'Encrypt file',
+				tabBarIcon: ({tintColor}) => (
+					<Icon name="lock" color={tintColor} size={24} />
+					// <Icon name="dashboard" color={tintColor} size={24} />
+				)
+			}
+		},
+		decrypt_file:{
+			screen: DecryptFileScreen,
+			navigationOptions:{
+				tabBarLabel: 'Decrypt file',
+				tabBarIcon: ({tintColor}) => (
+					<Icon name="lock-open" color={tintColor} size={24} />
+				)
+			}
+		},
+	}, {
+		shifting: false,
+		initialRouteName: 'home',
+		order: ['encrypt_file', 'home', 'decrypt_file'],
+		barStyleLight:{
+			backgroundColor: XEO_BLUE,
+			borderTopWidth: 2,
+			borderStyle: 'solid',
+			borderColor: '#d0cfd0',
+			paddingBottom: 5,
+
+		}
+	}
+);
+
+
+const Container = createAppContainer(BottomNavigator);
+
+export default Container;
---
+++ screens/main-screen.js
@@ -0,0 +1,23 @@
+import React, {Component} from "react";
+import {
+	Text,
+	View
+} from "react-native";
+
+
+export default class MainScreen extends Component{
+	constructor() {
+		super();
+	}
+
+	render() {
+		return(
+			<View>
+				<Text>
+					ok
+				</Text>
+			</View>
+		)
+	}
+
+}
